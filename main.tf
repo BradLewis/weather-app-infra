3b6e03dc619442bf63213dc65ceac63ac71ce9ee
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2022-04-09 17:52:52 -0400

    Reworking the deployment

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -36,16 +35,13 @@ data "aws_subnets" "all" {
 }
 
 module "weather_app_aurora_mysql" {
-  source = "github.com/terraform-aws-modules/terraform-aws-rds-aurora?ref=v3.5.0"
+  source = "github.com/terraform-aws-modules/terraform-aws-rds-aurora?ref=v6.2.0"
 
   name              = "${local.name}-mysql"
   engine            = "aurora-mysql"
   engine_mode       = "serverless"
   engine_version    = "5.7.mysql_aurora.2.07.1"
   storage_encrypted = true
-
-  replica_scale_enabled = false
-  replica_count         = 0
 
   subnets               = data.aws_subnets.all.ids
   vpc_id                = data.aws_vpc.default.id
@@ -56,9 +52,6 @@ module "weather_app_aurora_mysql" {
   apply_immediately   = true
   skip_final_snapshot = true
 
-  db_parameter_group_name         = aws_db_parameter_group.weather_app.id
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.weather_app.id
-
   scaling_configuration = {
     auto_pause               = true
     min_capacity             = 1
@@ -68,28 +61,17 @@ module "weather_app_aurora_mysql" {
   }
 }
 
-resource "aws_db_parameter_group" "weather_app" {
-  name        = "${local.name}-aurora-db-mysql-parameter-group"
-  family      = "aurora-mysql5.7"
-  description = "${local.name}-aurora-db-mysql-parameter-group"
-}
-
-resource "aws_rds_cluster_parameter_group" "weather_app" {
-  name        = "${local.name}-aurora-mysql-cluster-parameter-group"
-  family      = "aurora-mysql5.7"
-  description = "${local.name}-aurora-mysql-cluster-parameter-group"
-}
-
 resource "aws_secretsmanager_secret" "weather_app_db_credentials" {
   name = "${local.name}-aurora-db-master-credentials"
 }
 
 resource "aws_secretsmanager_secret_version" "weather_app_db_credentials" {
-  secret_id     = aws_secretsmanager_secret.weather_app_db_credentials.id
+  secret_id = aws_secretsmanager_secret.weather_app_db_credentials.id
   secret_string = jsonencode(
     {
-      username = module.weather_app_aurora_mysql.this_rds_cluster_master_username
-      password = module.weather_app_aurora_mysql.this_rds_cluster_master_password
+      username = module.weather_app_aurora_mysql.cluster_master_username
+      password = module.weather_app_aurora_mysql.cluster_master_password
+      host     = module.weather_app_aurora_mysql.cluster_endpoint
     }
   )
 }
