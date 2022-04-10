651d1f66a747f419ad5ca2a826f63fd923d0a020
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2022-04-10 10:23:40 -0400

    Creating and using a module for the vpc endpoint configuration

---
+++ modules/vpc-endpoint/main.tf
@@ -0,0 +1,40 @@
+
+resource "aws_vpc_endpoint" "this" {
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.${var.region}.${var.service_name}"
+  vpc_endpoint_type   = var.vpc_endpoint_type
+  private_dns_enabled = var.private_dns_enabled
+
+  security_group_ids = [
+    var.vpc_endpoint_sg_id,
+  ]
+}
+
+resource "aws_vpc_endpoint_subnet_association" "this" {
+  count = length(var.subnet_ids)
+
+  vpc_endpoint_id = aws_vpc_endpoint.this.id
+  subnet_id       = var.subnet_ids[count.index]
+}
+
+resource "aws_security_group_rule" "allowed_to_vpc_endpoint" {
+  count = length(var.allowed_security_group_ids)
+
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = var.allowed_security_group_ids[count.index]
+  source_security_group_id = var.vpc_endpoint_sg_id
+}
+
+resource "aws_security_group_rule" "vpc_endpoint_to_allowed" {
+  count = length(var.allowed_security_group_ids)
+
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = var.allowed_security_group_ids[count.index]
+  source_security_group_id = var.vpc_endpoint_sg_id
+}
--- network.tf
+++ network.tf
@@ -1,26 +1,8 @@
 
 resource "aws_security_group" "vpc_endpoint_sg" {
-  name_prefix      = "vpc-endpoint-"
+  name_prefix = "vpc-endpoint-"
   vpc_id      = data.aws_vpc.default.id
   description = "security group for accessing VPC endpoints"
-}
-
-resource "aws_vpc_endpoint" "secrets_manager" {
-  vpc_id              = data.aws_vpc.default.id
-  service_name        = "com.amazonaws.${local.region}.secretsmanager"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-
-  security_group_ids = [
-    aws_security_group.vpc_endpoint_sg.id,
-  ]
-}
-
-resource "aws_vpc_endpoint_subnet_association" "secrets_manager" {
-  count = length(data.aws_subnets.all.ids)
-
-  vpc_endpoint_id = aws_vpc_endpoint.secrets_manager.id
-  subnet_id       = data.aws_subnets.all.ids[count.index]
 }
 
 resource "aws_security_group" "setup_lambda_sg" {
@@ -29,20 +11,17 @@ resource "aws_security_group" "setup_lambda_sg" {
   description = "security group for the setup lambda"
 }
 
-resource "aws_security_group_rule" "setup_lambda_to_vpc_endpoint" {
-  type                     = "egress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.setup_lambda_sg.id
-  source_security_group_id = aws_security_group.vpc_endpoint_sg.id
-}
+module "secretsmanager_vpc_endpoint" {
+  source = "./modules/vpc-endpoint"
 
-resource "aws_security_group_rule" "vpc_endpoint_allow_setup_lambda" {
-  type                     = "ingress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.setup_lambda_sg.id
-  security_group_id        = aws_security_group.vpc_endpoint_sg.id
+  region              = local.region
+  vpc_id              = data.aws_vpc.default.id
+  service_name        = "secretsmanager"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  vpc_endpoint_sg_id  = aws_security_group.vpc_endpoint_sg.id
+  subnet_ids          = data.aws_subnets.all.ids
+  allowed_security_group_ids = [
+    aws_security_group.setup_lambda_sg.id
+  ]
 }
