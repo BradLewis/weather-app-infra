eda507c988ebecc0b3abb86ced47370dfde5ef86
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2022-04-13 18:56:10 -0400

    Adding SSM for vpc endpoint

--- network.tf
+++ network.tf
@@ -26,6 +26,20 @@ resource "aws_security_group" "weather_api_lambda" {
   }
 }
 
+module "ssm_vpc_endpoint" {
+  source = "./modules/vpc-endpoint"
+
+  region              = local.region
+  vpc_id              = data.aws_vpc.default.id
+  service_name        = "ssm"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  vpc_endpoint_sg_id  = aws_security_group.vpc_endpoint_sg.id
+  subnet_ids          = data.aws_subnets.all.ids
+
+  allowed_security_group_ids = []
+}
+
 module "secretsmanager_vpc_endpoint" {
   source = "./modules/vpc-endpoint"
 
