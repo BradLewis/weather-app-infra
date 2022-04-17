84cc4225c24cc87dd5260df5722f88e6ec48327f
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2022-04-17 14:18:24 -0400

    Adding api gateway stage

--- api.tf
+++ api.tf
@@ -3,6 +3,11 @@ resource "aws_apigatewayv2_api" "weather_app_api" {
   protocol_type = "HTTP"
 }
 
+resource "aws_apigatewayv2_stage" "weather_app_stage" {
+  api_id = aws_apigatewayv2_api.weather_app_api.id
+  name   = "$default"
+}
+
 resource "aws_ssm_parameter" "api_id" {
   name  = "/${local.name}/http-api-id"
   type  = "String"
